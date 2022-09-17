07f833822193f9256aface1023a98cdf79f8e105
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-09-17 16:59:45 +0700

    update

--- views/login.php
+++ views/login.php
@@ -9,7 +9,7 @@ if (isset($_POST['login'])) {
     if ($_POST['username'] == $user && $_POST['password'] == $pass) {
         header('Location: index.php');
     } else {
-        $pesan = "Username atau Password salah";
+        $pesan = "<div class='alert alert-danger'>Username atau Password salah</div>";
     }
 }
 ?>
@@ -17,22 +17,20 @@ if (isset($_POST['login'])) {
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-3 position-absolute top-50 start-50 translate-middle">
-            <div class="alert alert-danger">
-                <?= $pesan ?>
-            </div>
+            <?= $pesan ?>
             <div class="card p-3">
                 <h1>Login.</h1>
                 <form action="" method="POST">
                     <div class="form-group mb-3">
                         <label for="username">Username</label>
-                        <input class="form-control" type="text" name="username" placeholder="Username atau email" />
+                        <input class="form-control" type="text" name="username" placeholder="Username atau email">
                     </div>
     
                     <div class="form-group mb-3">
                         <label for="password">Password</label>
-                        <input class="form-control" type="password" name="password" placeholder="Password" />
+                        <input class="form-control" type="password" name="password" placeholder="Password">
                     </div>
-                    <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" />
+                    <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk">
                 </form>
             </div>
         </div>
